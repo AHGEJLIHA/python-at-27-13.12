ac1f8e968f88aea88d82405ba1f0b299c08906d5
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-08 20:52:58 +0500

    The second step

--- filter.py
+++ filter.py
@@ -1,28 +1,40 @@
 from PIL import Image
 import numpy as np
+
+def get_grey_shade(i, width, width_mosaic, j, height, height_mosaic, matrix, grey):
+    for cur_width in range(i, min(width, i + width_mosaic)):
+        for cur_height in range(j, min(height, j + height_mosaic)):
+            red = matrix[cur_width][cur_height][0]
+            green = matrix[cur_width][cur_height][1]
+            blue = matrix[cur_width][cur_height][2]
+            grey += (int(red) + int(green) + int(blue)) / 3
+    return grey
+
+def create_new_picture(i, width, width_mosaic, j, height, height_mosaic, matrix, greyscale):
+    for cur_width in range(i, min(width, i + width_mosaic)):
+        for cur_height in range(j, min(height, j + height_mosaic)):
+            for cur_RGB in range(0, 3):
+                matrix[cur_width][cur_height][cur_RGB] = int(grey // 50) * 50 * greyscale
+
+    return matrix
+
 img = Image.open("img2.jpg")
 matrix = np.array(img)
 width = len(matrix)
 height = len(matrix[1])
 
+input_data = input().split(" ")
+greyscale = int(input())
+width_mosaic = int(input_data[0])
+height_mosaic = int(input_data[1])
+
 i = 0
 while i < width - 1:
     j = 0
     while j < height - 1:
-        grey = 0
-        for i1 in range(i, i + 10):
-            for j1 in range(j, j + 10):
-                red = matrix[i1][j1][0]
-                green = matrix[i1][j1][1]
-                blue = matrix[i1][j1][2]
-                grey += (int(red) + int(green) + int(blue)) / 3
-        grey = int(grey // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                matrix[n][n1][0] = int(grey // 50) * 50
-                matrix[n][n1][1] = int(grey // 50) * 50
-                matrix[n][n1][2] = int(grey // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(matrix)
+        grey = int(get_grey_shade(i, width, width_mosaic, j, height, height_mosaic, matrix, 0) // 100)
+        new_matrix = create_new_picture(i, width, width_mosaic, j, height, height_mosaic, matrix, greyscale)
+        j = j + height_mosaic
+    i = i + width_mosaic
+res = Image.fromarray(new_matrix)
 res.save('res.jpg')

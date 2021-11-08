7b77ce4855879621755c91fde74792b5e2937a63
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-08 22:10:26 +0500

    The third step

--- filter.py
+++ filter.py
@@ -1,21 +1,22 @@
 from PIL import Image
 import numpy as np
 
-def get_grey_shade(i, width, width_mosaic, j, height, height_mosaic, matrix, grey):
-    for cur_width in range(i, min(width, i + width_mosaic)):
-        for cur_height in range(j, min(height, j + height_mosaic)):
-            red = matrix[cur_width][cur_height][0]
-            green = matrix[cur_width][cur_height][1]
-            blue = matrix[cur_width][cur_height][2]
-            grey += (int(red) + int(green) + int(blue)) / 3
-    return grey
 
-def create_new_picture(i, width, width_mosaic, j, height, height_mosaic, matrix, greyscale):
-    for cur_width in range(i, min(width, i + width_mosaic)):
-        for cur_height in range(j, min(height, j + height_mosaic)):
-            for cur_RGB in range(0, 3):
-                matrix[cur_width][cur_height][cur_RGB] = int(grey // 50) * 50 * greyscale
+def get_grayscale(i, width, width_mosaic, j, height, height_mosaic):
+    gray = 0
+    for cur_width in range(i, min(i + width // width_mosaic, width)):
+        for cur_height in range(j, min(j + height // height_mosaic, height)):
+            R = matrix[cur_width][cur_height][0]
+            G = matrix[cur_width][cur_height][1]
+            B = matrix[cur_width][cur_height][2]
+            gray += (int(R) + int(G) + int(B)) / 3
+    return int(gray // 100)
 
+
+def create_grey_picture(x, width, width_mosaic, y, height, height_mosaic, grayscale_step, grayscale):
+    for cur_width in range(x, min(x + width // width_mosaic, width)):
+        for cur_height in range(y, min(y + height // height_mosaic, height)):
+            matrix[cur_width][cur_height] = [int(grayscale // 50 * grayscale_step) * grayscale_step * 50] * 3
     return matrix
 
 img = Image.open("img2.jpg")
@@ -23,18 +24,17 @@ matrix = np.array(img)
 width = len(matrix)
 height = len(matrix[1])
 
-input_data = input().split(" ")
-greyscale = int(input())
-width_mosaic = int(input_data[0])
-height_mosaic = int(input_data[1])
+width_mosaic = int(input())
+height_mosaic = int(input())
+grayscale_step = int(input())
 
 i = 0
 while i < width - 1:
     j = 0
     while j < height - 1:
-        grey = int(get_grey_shade(i, width, width_mosaic, j, height, height_mosaic, matrix, 0) // 100)
-        new_matrix = create_new_picture(i, width, width_mosaic, j, height, height_mosaic, matrix, greyscale)
-        j = j + height_mosaic
-    i = i + width_mosaic
-res = Image.fromarray(new_matrix)
+        grayscale = get_grayscale(i, width, width_mosaic, j, height, height_mosaic)
+        matrix = create_grey_picture(i, width, width_mosaic, j, height, height_mosaic, grayscale_step, grayscale)
+        j = j + height // height_mosaic
+    i = i + width // width_mosaic
+res = Image.fromarray(matrix)
 res.save('res.jpg')
